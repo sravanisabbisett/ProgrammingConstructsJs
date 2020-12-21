1409a06a7a81a5971e9c1b9e2959ee11169a0c8e
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-12-21 17:23:20 +0530

    ADDED:Sequence problem statements

--- Sequence/GenerateRandomNumber.js
+++ Sequence/GenerateRandomNumber.js
@@ -1,1 +1,21 @@
-console.log("Welcome to prgramming constructs");
+//Generate Random Value
+let randomNumber=Math.floor(Math.random()*10);
+console.log("Random number is "+randomNumber);
+
+//Generate Randam Dice Number
+let diceRandomNumber=Math.floor(Math.random()*10)%6+1;
+console.log("Random number is "+diceRandomNumber);
+
+//AddTwoRandomDiceNumber
+let addDiceValue=Math.floor(Math.random()*10)%6+1+Math.floor(Math.random()*10)%6+1;
+console.log("Adding two Random Dice Number:"+addDiceValue);
+
+//Add five 3 digit Random number find sum and average
+let sum=0;
+for(let i=0;i<5;i++){
+    let randomThreeDigitNumber=Math.floor(Math.random()*90+10);
+    console.log("Random Number is:"+randomThreeDigitNumber);
+    sum+=randomThreeDigitNumber;
+}
+console.log("Sum of 5 three digit Random Number is:"+sum);
+console.log("Average of 5 three didit random is:"+(sum/5));
---
+++ Sequence/unitConservion.js
@@ -0,0 +1,13 @@
+//ConvertFeetToInch
+let OneFeetToInch=1/12;
+let convertVaraible=42
+console.log("Calculate 42 inches in feet:"+(convertVaraible*OneFeetToInch));
+
+//Rectangular polt of 60ft*40ft in meters
+//1feet=0.3048m
+let CalculateFtTometers=(60*40*0.3048);
+console.log("Calculating 60ft*40ft in meters:"+CalculateFtTometers);
+
+//calculate the area of 25 plots
+let areaOf25Plots=(((60*40)/43650)*25);
+console.log("Calculating 25 plots:"+areaOf25Plots);
